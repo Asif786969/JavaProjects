9904b9c24afad138af70b4c8716132f6a8d700a0
Mohammad Asif <mohasif6@publicisgroupe.net>
2022-10-18 13:53:41 +0530

    created first spring boot

---
+++ testingphase1/src/main/java/com/example/testingphase1/Testingphase1Application.java
@@ -0,0 +1,13 @@
+package com.example.testingphase1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Testingphase1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Testingphase1Application.class, args);
+	}
+
+}
